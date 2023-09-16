f7340eacb517ba9d79499db2faaed742b67146d9
harrywebb21 <harrywebb21@gmail.com>
2023-09-16 14:09:29 +0100

    started the player page for mobile

--- src/routes/players/[player]/+page.svelte
+++ src/routes/players/[player]/+page.svelte
@@ -30,15 +30,50 @@
 			console.error('Error:', error);
 		}
 	});
+	console.log('playerStats:', data.playerData.athletes[1].items);
+	console.log('packer data:', data.packerData);
 </script>
 
-{#if playerStats}
-	<p>{playerStats.$ref}</p>
-{:else}
-	<p>Loading player stats...</p>
-{/if}
+<div class="wrapper w-screen relative overflow-hidden -mt-8">
+	<div class="banner-yellow" />
+	<div class="banner-green" />
+	<div class="banner-white" />
+	<img class="logo" src={data.packerData.team.logos[0].href} alt="" />
+	{#each data.playerData.athletes[0].items as players}
+		{#if players.id === $page.params.player}
+			<div class="player-container">
+				<div class="headshot-container">
+					<p class="jersey-stat-mobile">{players.jersey}</p>
+					<img class="headshot" src={players.headshot.href} alt="" />
+				</div>
+				<div class="">
+					<p class="player-name">{players.displayName}</p>
+					<div class="">
+						<p class="age-stat">AGE: {players.age}</p>
+						<p class="height-stat">HEIGHT: {players.displayHeight}</p>
+						<p class="weight-stat">WEIGHT: {players.displayWeight}</p>
+					</div>
+				</div>
+				<p class="jersey-stat">#{players.jersey}</p>
+				<p class="position-stat">{players.position.abbreviation}</p>
+			</div>
+			<p class="position-stat-mobile">{players.position.abbreviation}</p>
 
-<div class="">
+			<div class="player-info-container">
+				<p class="age-stat-mobile">AGE: {players.age}</p>
+				<p class="height-stat-mobile">HEIGHT: {players.displayHeight}</p>
+				<p class="weight-stat-mobile">WEIGHT: {players.displayWeight}</p>
+				<p class="experience-stat-mobile">EXPERIENCE: {players.experience.years} Years</p>
+				<p class="college-stat-mobile">COLLEGE: {players.college.shortName}</p>
+				<p class="hometown-stat-mobile">
+					HOMETOWN: {players.birthPlace.city}, {players.birthPlace.state}
+				</p>
+			</div>
+		{/if}
+	{/each}
+</div>
+
+<!-- <div class="">
 	{#each data.playerData.athletes[0].items as players}
 		{#if players.id === $page.params.player}
 			<img src={players.headshot.href} alt="" />
@@ -84,4 +119,147 @@
 			<p>{players.displayWeight}</p>
 		{/if}
 	{/each}
-</div>
+</div> -->
+
+<style>
+	.banner-green {
+		background-color: #203731;
+		height: 500px;
+		width: 100%;
+		position: absolute;
+		z-index: 3;
+		top: -35%;
+		left: 0%;
+		transform: skewY(10deg);
+	}
+
+	.banner-white {
+		background-color: #fff;
+		height: 500px;
+		width: 100%;
+		position: absolute;
+		z-index: 2;
+		top: -30%;
+		left: 0%;
+		transform: skewY(10deg);
+		border-bottom: 2px solid white;
+	}
+
+	.banner-yellow {
+		background-color: #ffb612;
+		height: 500px;
+		width: 100%;
+		position: absolute;
+		top: 10%;
+		border-bottom: 2px solid white;
+	}
+
+	.logo {
+		position: absolute;
+		top: 55%;
+		right: 20%;
+		transform: translateX(50%);
+		z-index: 1;
+		height: 100px;
+	}
+
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+		height: 600px;
+	}
+
+	.headshot-container {
+		position: relative;
+		width: 100%;
+		height: 150px;
+	}
+
+	.jersey-stat-mobile {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		font-size: 11rem;
+		font-weight: 800;
+		color: rgba(255, 255, 255, 0.3);
+		z-index: 3;
+	}
+
+	.position-stat-mobile {
+		position: absolute;
+		top: 45.5%;
+		right: 20%;
+		transform: translate(50%, -50%);
+		font-size: 2rem;
+		font-weight: 800;
+		background: var(--packer-yellow);
+		border-radius: 100vh;
+		padding: 0.25rem 1.5rem;
+		z-index: 3;
+	}
+
+	.player-info-container {
+		position: absolute;
+		bottom: 10%;
+		left: 5%;
+
+		z-index: 3;
+		display: flex;
+		font-size: 1.5rem;
+		font-weight: 800;
+		flex-direction: column;
+		justify-content: start;
+		align-items: start;
+	}
+	.player-container {
+		position: absolute;
+		top: 3%;
+		left: 50%;
+		transform: translateX(-50%);
+		z-index: 4;
+		border: 1px solid rgba(255, 255, 255, 0.3);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		width: 85%;
+		background: rgba(255, 255, 255, 0.1);
+		border-radius: 20px;
+		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+		backdrop-filter: blur(10px);
+		padding: 0.25rem;
+	}
+
+	.headshot {
+		width: 200px;
+		height: 150px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: 10;
+	}
+	.player-name {
+		font-size: 2.5rem;
+		font-weight: 800;
+		width: 100%;
+	}
+	.age-stat,
+	.height-stat,
+	.weight-stat {
+		font-size: 1.5rem;
+		font-weight: 700;
+		display: none;
+	}
+
+	.jersey-stat,
+	.position-stat {
+		font-size: 5rem;
+		font-weight: 700;
+		margin: 0;
+		display: none;
+	}
+</style>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,180 +1,63 @@
-<script lang="ts">
+<script>
 	import GlassCard from '$lib/components/cards/GlassCard.svelte';
 
 	export let data;
-	console.log(data.standingsData.content.standings.groups[0].groups[0]);
+	console.log('packer data:', data.packerData);
 </script>
 
-<div class="page flex flex-col gap-5 w-full">
-	{#each data.standingsData.content.standings.groups as regions}
-		<p class="region text-4xl font-black">{regions.abbreviation}</p>
-		{#each regions.groups as divisions}
-			<div class="table-wrapper">
-				<table>
-					<tr class="header-row">
-						<th>{divisions.abbreviation}</th>
-						<th class="main-stat-title"
-							>{divisions.standings.entries[0].stats[0].shortDisplayName}</th
-						>
-						<th class="main-stat-title"
-							>{divisions.standings.entries[0].stats[1].shortDisplayName}</th
-						>
-						<th class="main-stat-title"
-							>{divisions.standings.entries[0].stats[2].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[3].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[4].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[5].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[6].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[7].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[8].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[9].shortDisplayName}</th
-						>
-						<th class="optional-stat-title"
-							>{divisions.standings.entries[0].stats[10].shortDisplayName}</th
-						>
-						<th class="main-stat-title"
-							>{divisions.standings.entries[0].stats[11].shortDisplayName}</th
-						>
-					</tr>
-
-					{#each divisions.standings.entries as teams}
-						<tr class="team-row">
-							<th class="team-header items-center">
-								<img class="team-logo" src={teams.team.logos[0].href} alt="" />
-
-								<p class="desktop-teams">
-									{teams.team.displayName}
-								</p>
-								<p class="mobile-teams">
-									{teams.team.name}
-								</p>
-							</th>
-							<td class="main-stat">{teams.stats[0].displayValue}</td>
-							<td class="main-stat">{teams.stats[1].displayValue}</td>
-							<td class="main-stat">{teams.stats[2].displayValue}</td>
-							<td class="optional-stat">{teams.stats[3].displayValue}</td>
-							<td class="optional-stat">{teams.stats[4].displayValue}</td>
-							<td class="optional-stat">{teams.stats[5].displayValue}</td>
-							<td class="optional-stat">{teams.stats[6].displayValue}</td>
-							<td class="optional-stat">{teams.stats[7].displayValue}</td>
-							<td class="optional-stat">{teams.stats[8].displayValue}</td>
-							<td class="optional-stat">{teams.stats[9].displayValue}</td>
-							<td class="optional-stat">{teams.stats[10].displayValue}</td>
-							<td class="main-stat">{teams.stats[11].displayValue}</td>
-						</tr>
-					{/each}
-				</table>
-			</div>
-		{/each}
-	{/each}
+<div class="wrapper h-[300px] w-screen relative overflow-hidden -mt-8">
+	<div class="banner-yellow" />
+	<div class="banner-green" />
+	<div class="banner-white" />
+	<img class="logo h-[400px]" src={data.packerData.team.logos[0].href} alt="" />
 </div>
 
 <style>
-	.region {
-		background: var(--packer-yellow);
-		padding: 0.5rem 1rem;
-		border-radius: 20px 20px 0 0;
-	}
-	table {
+	.banner-green {
+		background-color: #203731;
+		height: 300px;
 		width: 100%;
-		border-collapse: collapse;
+		position: absolute;
+		z-index: 3;
+		top: -0px;
+		left: -25%;
+		transform: skewX(-20deg);
+		border-bottom: 2px solid white;
 	}
-	.team-row {
-		border: 1px solid rgba(255, 255, 255, 0.3);
-		background: rgba(255, 255, 255, 0.1);
-	}
-	.team-row > td {
-		padding: 0.5rem 1rem;
-		text-align: center;
-	}
-	.table-wrapper {
+
+	.banner-white {
+		background-color: #fff;
+		height: 300px;
 		width: 100%;
-		border-collapse: collapse;
-		background: rgba(255, 255, 255, 0.1);
-		border: 1px solid rgba(255, 255, 255, 0.3);
-		padding: 0.5rem 1rem;
+		position: absolute;
+		z-index: 2;
+		top: 0px;
+		left: -20%;
+		transform: skewX(-20deg);
+		border-bottom: 2px solid white;
+	}
+
+	.banner-yellow {
+		background-color: #ffb612;
+		height: 300px;
+		width: 100%;
+		position: absolute;
+		top: 0px;
+		border-bottom: 2px solid white;
+	}
+
+	.logo {
+		position: absolute;
+		top: 50%;
+		right: 0%;
+		transform: translateX(50%) translateY(-50%);
+		z-index: 1;
+	}
+
+	.wrapper {
 		display: flex;
-		align-items: center;
 		justify-content: center;
-	}
-	th {
-		padding: 0.5rem 1rem;
-		text-align: center;
-	}
-
-	.optional-stat-title {
-		display: none;
-	}
-
-	.optional-stat {
-		display: none;
-	}
-
-	.team-header {
-		display: flex;
-		flex-direction: column;
 		align-items: center;
-		justify-content: center;
-		font-size: clamp(0.75rem, 1vw, 1rem);
-	}
-
-	.desktop-teams {
-		display: none;
-	}
-
-	.team-logo {
-		width: 30px;
-		height: 30px;
-	}
-
-	@media (min-width: 768px) {
-		.team-header {
-			flex-direction: row;
-			justify-content: start;
-			gap: 1rem;
-			font-size: clamp(0.75rem, 1vw + 1rem, 1.5rem);
-		}
-		.desktop-teams {
-			display: block;
-		}
-		.mobile-teams {
-			display: none;
-		}
-	}
-
-	@media (min-width: 1024px) {
-		.optional-stat-title {
-			display: table-cell;
-		}
-
-		.optional-stat {
-			display: table-cell;
-		}
-		.team-header {
-			flex-direction: row;
-			justify-content: start;
-			gap: 1rem;
-			font-size: clamp(0.75rem, 1vw + 1rem, 1.5rem);
-		}
-		.desktop-teams {
-			display: block;
-		}
-		.mobile-teams {
-			display: none;
-		}
+		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
 	}
 </style>
